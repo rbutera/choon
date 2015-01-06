79ef871a24db03ab7aa6c234761a7a9fc25142f4
Rai Butera <rai@rbutera.com>
2015-01-06 14:23:57 +0000

    improved terminal output

--- choon.js
+++ choon.js
@@ -31,7 +31,7 @@ if (!youtubeUrl) {
   throw new Error('usage: choon <url> <destination directory>');
 } else {
   // validate url as youtube
-  console.log(chalk.bold('choon: ') + 'using offliberty to extract audio from ' + youtubeUrl + '.');
+  console.log(chalk.white.bgBlack.bold('choon: ') + 'using offliberty to extract audio from ' + chalk.underline(youtubeUrl));
 
   // extract uuid from youtube url
   youtubeId = extractYoutubeId(youtubeUrl);
@@ -41,14 +41,14 @@ var download = function (url, dest) {
   if (!url || !dest) {
     throw new Error('Download: missing argument(s)');
   } else {
-    console.log('Downloading ' + url + ' as ' + dest);
+    console.log('Downloading ' + chalk.underline(url) + ' as ' + chalk.bold(dest));
     // Note that the options argument is optional
     progress(request(url), {
         throttle: 2000,  // Throttle the progress event to 2000ms, defaults to 1000ms
         delay: 1000      // Only start to emit after 1000ms delay, defaults to 0ms
     })
     .on('progress', function (state) {
-        console.log(chalk.bold(state.percent) + '% complete');
+        console.log(chalk.bold(state.percent) + '% ' + chalk.dim('complete'));
     })
     .on('error', function (err) {
         throw err;
@@ -61,7 +61,7 @@ var download = function (url, dest) {
        if(err){
          throw err;
        }
-       console.log(chalk.green.bgBlack('Finished!'));
+       console.log(chalk.green.bgBlack('Finished! Enjoy your choon!'));
        process.exit();
     });
   }
@@ -79,11 +79,11 @@ if (youtubeId) {
       throw new Error('ERROR: Could not generate download Url');
     } else {
       if(!process.argv[3]){
-        console.log('No destination directory specified.... defaulting to ~/Downloads/');
+        console.log(chalk.dim('No destination directory specified.... defaulting to ~/Downloads/'));
       }
 
       var finalDestination = downloadDirectory + '/' + youtubeId + '.mp3'; // no items, fox only, final destination
-      console.log(chalk.dim('File will be downloaded to ' + finalDestination));
+      console.log(chalk.dim('File will be downloaded to ' + chalk.underline(finalDestination)));
       download(downloadUrl, finalDestination);
 
     }
